076934c82d26a9471626382e6d7c05b896bc349b
sandalfon <sandalfon@gmail.com>
2018-06-11 14:45:37 +0200

    alignement

--- src/main/scala/pkm/models/Duel.scala
+++ src/main/scala/pkm/models/Duel.scala
@@ -210,10 +210,7 @@ object Duel {
     }
     true
   }
-
-  def roundLog = {
-
-  }
+  
 
   override def toString: String = "P1: \n" + varStat1 + "\nP2: \n" + varStat2
 
--- src/main/scala/pkm/models/Pokemon.scala
+++ src/main/scala/pkm/models/Pokemon.scala
@@ -3,22 +3,26 @@ package pkm.models
 import io.circe.Decoder
 
 case class Pokemon(
-  number: Int,
-  name: String,
-  `type`: String,
-  stats: PokemonStats,
-  basicAttack: BasicAttack,
-  powerAttack: PowerAttack
-) {
+                    number: Int,
+                    name: String,
+                    `type`: String,
+                    stats: PokemonStats,
+                    basicAttack: BasicAttack,
+                    powerAttack: PowerAttack
+                  ) {
   override def toString = s"""Pokemon $number "$name" (${`type`}) - HP: ${stats.hp}"""
 }
 
 case class PokemonType(name: String) extends AnyVal
+
 case class PokemonStats(hp: Int, speed: Int)
+
 case class BasicAttack(name: String, `type`: String, damage: Int)
+
 case class PowerAttack(name: String, `type`: String, damage: Int, cost: Int)
 
 trait PokemonDecoder {
+
   import io.circe.generic.semiauto._
 
   implicit protected val basicAttackDecoder = deriveDecoder[BasicAttack]
